8fc3ffe49830035497705f0adaf1483d71194d13
wenjialu <izoeyz@163.com>
2020-09-23 19:47:33 +0800

    gittalk-js

--- index.html/js/grayscale.js
+++ index.html/js/grayscale.js
@@ -48,3 +48,17 @@
 jQuery.htmlPrefilter = function( html ) {
 	return html;
 };
+
+
+//comment gittalk
+const gitalk = new Gitalk({
+  clientID: 'GitHub Application Client ID',
+  clientSecret: 'GitHub Application Client Secret',
+  repo: 'GitHub repo',
+  owner: 'GitHub repo owner',
+  admin: ['GitHub repo owner and collaborators, only these guys can initialize github issues'],
+  id: location.pathname,      // Ensure uniqueness and length less than 50
+  distractionFreeMode: false  // Facebook-like distraction free mode
+})
+ 
+gitalk.render('gitalk-container');
